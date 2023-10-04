7e71ab94709fc28b1e32c6ab7fbc9de14ceb9c60
unknown <martin.jernstrom1@gmail.com>
2023-10-04 20:11:40 +0200

    task5

--- src/com/martin/demo/Main.java
+++ src/com/martin/demo/Main.java
@@ -6,5 +6,10 @@ public class Main {
         Ford car = new Ford();
         car.hasWheels();
         car.sound();
+
+        car.transport();
+
+        Ship boat = new Ship();
+        boat.transport();
     }
 }
--- src/com/martin/demo/Vehicle.java
+++ src/com/martin/demo/Vehicle.java
@@ -4,4 +4,8 @@ public interface Vehicle {
 
     void hasWheels();
     void sound();
+
+    default void transport() {
+        System.out.println("Transporting");
+    }
 }
