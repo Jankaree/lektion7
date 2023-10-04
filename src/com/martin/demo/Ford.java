066b6525b923d6116ef16e256595305f510fe914
unknown <martin.jernstrom1@gmail.com>
2023-10-04 20:08:42 +0200

    task3

--- src/com/martin/demo/Ford.java
+++ src/com/martin/demo/Ford.java
@@ -3,11 +3,11 @@ package com.martin.demo;
 public class Ford implements Vehicle{
     @Override
     public void hasWheels() {
-
+        System.out.println("yes there are wheels");
     }
 
     @Override
     public void sound() {
-
+        System.out.println("wroom");
     }
 }
---
+++ src/com/martin/demo/Main.java
@@ -0,0 +1,10 @@
+package com.martin.demo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ford car = new Ford();
+        car.hasWheels();
+        car.sound();
+    }
+}
